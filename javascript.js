221c4540115ceeb9da0edfacec0844c1d6cec50a
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-04-28 10:29:30 +0200

    Fixade en ny funktion till sub menyerna

--- javascript.js
+++ javascript.js
@@ -2,6 +2,19 @@ function oppnaMeny() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+let menuItems = document.querySelectorAll(".has-sub-menu");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", showSubMenu);
+})
+
+function showSubMenu(event) {
+  console.log(event.target);
+  console.log(event.target.getAttribute("data-linked-menu"));
+  let targetMenu = "#" + event.target.getAttribute("data-linked-menu");
+  document.querySelector(targetMenu).classList.toggle("show");
+}
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
