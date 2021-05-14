005b404751882805913cb310d6a140ba8124e24c
Nikolaj Goj <nikolaj.goj@elev.ga.ntig.se>
2021-05-14 14:08:03 +0200

    Fixade med javascript koden

--- javascript.js
+++ javascript.js
@@ -17,23 +17,13 @@ menuItems.forEach((item) => {
 })
 
 function showSubMenu(event) {
+  subMenus.forEach((item) => {
+    item.classList.remove("show");
+  })
   console.log(event.target);
   console.log(event.target.getAttribute("data-linked-menu"));
   let targetMenu = "#" + event.target.getAttribute("data-linked-menu");
   document.querySelector(targetMenu).classList.toggle("show");
 }
 
-/*
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-*/
+
